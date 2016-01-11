8905bbc68120d99940c30f9174ddd7654db3c8b8
Teddy Kentor <tkentor@gmail.com>
2016-01-11 12:28:16 -0500

    working on homepage

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -45,7 +45,7 @@ class ArticlesController < ApplicationController
 
 private
   def article_params
-    params.require(:article).permit(:title, :text)
+    params.require(:article).permit(:title, :body, :takeaway, :action, :sources, :pictures)
   end
 
 end
