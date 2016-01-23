77d0c27e221be64fe32646ae2a4d8b1249fc5582
Teddy Kentor <tkentor@gmail.com>
2016-01-23 14:36:54 -0500

    random subtext

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,17 +17,24 @@
 $(document).ready(function(){
 
   $(".article").click(function(){
-      $(this).find(".expand").toggle();
-      $(this).find(".click").toggle();
-      $(this).find(".time").toggleClass("time-right")
-    })
+    $(this).find(".expand").toggle();
+    $(this).find(".click").toggle();
+    $(this).find(".time").toggleClass("time-right")
+  })
 
   $(".story").click(function(){
-      $(this).find(".expand").toggle();
-      $(this).find(".click").toggle();
-      $(this).find(".time").toggleClass("time-right")
-    })
+    $(this).find(".expand").toggle();
+    $(this).find(".click").toggle();
+    $(this).find(".time").toggleClass("time-right")
+  })
 
   $("img[src='']").remove();
+
+  $(document).ready(function() {
+      var quotes = new Array("welcome to my snoop bloggy blog", "get rich or die blogging", "bloggy style. blah-blah-bloggy style.", "live free or blog hard", "eat. blog. repeat.", "it's a blog off.", "bloggers too can't not die in a freak gasoline fight accident.", "the blogs are in the computer.", "i love blog. bloggy blog blog. here it goes, down into my belly.", "wait. you changed your name, to McBloggin?", "blog. james blog.", "you like blogs? how you like them blogs.", "where we are going, we don't need blogs.", "rule #76: no excuses. blog like a champion", "not my blog. not hermione's blog. yours, harry."),
+      randno = quotes[Math.floor( Math.random() * quotes.length )];
+      $('.quote').text( randno );
+  });
+
 
 });
